4f5edc6f1a0bd376141989fa472f3b8f24e1ce62
BRYANT MONCADA CONTRERAS <bmoncada@universidadean.edu.co>
2026-01-19 13:00:00 -0500

    feat(products): define product entity and exceptions

---
+++ services/ms-products/src/main/java/com/linktic/msproducts/domain/port/out/ProductRepositoryPort.java
@@ -0,0 +1,11 @@
+package com.linktic.msproducts.domain.port.out;
+
+import com.linktic.msproducts.domain.model.Product;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProductRepositoryPort {
+  Product save(Product product);
+
+  Optional<Product> findById(UUID id);
+}
