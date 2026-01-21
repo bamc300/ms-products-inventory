fe28e4216ab9a609eea783e896b7541e58f33449
BRYANT MONCADA CONTRERAS <bmoncada@universidadean.edu.co>
2026-01-21 17:25:39 -0500

    refactor: remove unused import from PurchaseRequestDto

--- services/ms-inventory/src/main/java/com/linktic/msinventory/application/dto/PurchaseRequestDto.java
+++ services/ms-inventory/src/main/java/com/linktic/msinventory/application/dto/PurchaseRequestDto.java
@@ -3,7 +3,6 @@ package com.linktic.msinventory.application.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
